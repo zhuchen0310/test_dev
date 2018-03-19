72c009199e0edab34813e58d2a3b7bea9fcfc5f4
zhuchen0310 <448290415@qq.com>
2018-03-20 01:02:43 +0800

    html

--- jd_app/config.py
+++ jd_app/config.py
@@ -1,5 +1,27 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 # @Time    : 2018/3/17 上午2:15
+import redis
 
 DB_URI = "mysql://zhuchen:zhuchen@118.24.159.168:3306/jingfen_dev"
+
+SQLALCHEMY_DATABASE_URI = "mysql://zhuchen:zhuchen@118.24.159.168:3306/jingfen_dev"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+
+SECRET_KEY = 'TQ343dfsdf34+SDjjojlje343ET+?#$ODFDSFSD'
+
+# REDIS
+REDIS_HOST = "118.24.159.168"
+REDIS_PORT = 6379
+REDIS_DB = 10
+
+# flask-session 使用参数
+SESSION_TYPE = "redis"  # 利用redis 来保存session会话
+
+#
+SESSION_USE_SIGNER = True  # 为sesson_id进行签名
+SESSION_REDIS = redis.StrictRedis(host=REDIS_HOST, port=REDIS_PORT)  # redis 缓存设置
+
+#
+PERMANENT_SESSION_LIFETIME = 86400  # session数据的有效期 秒
